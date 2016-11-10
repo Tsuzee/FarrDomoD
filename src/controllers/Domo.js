7aee0e34ec454142524a98679576e05c36ea8a55
Darren Farr (RIT Student) <dmf6737@ad.rit.edu>
2016-11-09 19:34:57 -0500

    initial commit

---
+++ src/controllers/Domo.js
@@ -0,0 +1,41 @@
+const makerPage = (req, res) => {
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+
+    return res.render('app', {csrfToken: req.csrfToken(), domos: docs });
+  });
+};
+
+const models = require('../models');
+
+const Domo = models.Domo;
+
+const makeDomo = (req, res) => {
+  if (!req.body.name || !req.body.color || !req.body.age) {
+    return res.status(400).json({ error: 'RAWR!! Name, color, and age are required' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    color: req.body.color,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  return newDomo.save((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+
+    return res.json({ redirect: '/maker' });
+  });
+};
+
+module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
